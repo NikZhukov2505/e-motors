a3e8218f7419b1a5cea235b9ba16163cffa6c30b
numinex777 <borzenko605@gmail.com>
2022-12-05 15:42:36 +0600

    commit

--- src/Components/Catalog/index.jsx
+++ src/Components/Catalog/index.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
 import styles from './Catalog.module.css'
+import moreCatalog from './../../assets/moreCatalog.png'
+import carCatalog from './../../assets/carCatalog.png'
+import motoCatalog from './../../assets/motoCatalog.png'
+import samoCatalog from './../../assets/samoCatalog.png'
+import sircleCatalog from './../../assets/sircleCatalog.png'
 
 const Catalog = () => {
     return (
-        <div>
-
+        <div className={styles.container}>
+            <h2 className={styles.Catalog_title}>каталог</h2>
+            <div className={styles.Output_Catalog}>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={moreCatalog} alt="moreCatalog" />
+                    </div>
+                    <p>Все категории</p>
+                </div>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={carCatalog} alt="carCatalog" />
+                    </div>
+                    <p>Электромобили</p>
+                </div>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={motoCatalog} alt="motoCatalog" />
+                    </div>
+                    <p>Электробайки</p>
+                </div>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={samoCatalog} alt="samoCatalog" />
+                    </div>
+                    <p>Электротранспорт</p>
+                </div>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={sircleCatalog} alt="sircleCatalog" />
+                    </div>
+                </div>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={sircleCatalog} alt="sircleCatalog" />
+                    </div>
+                </div>
+                <div className={styles.card_categories}>
+                    <div className={styles.item_card}>
+                        <img src={sircleCatalog} alt="sircleCatalog" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import styles from './Footer.module.css'
-
+import logo from './../../assets/logo.png'
+import telegram from './../../assets/telegram.png'
+import whatsapp from './../../assets/whatsapp.png'
+import instagram from './../../assets/instagram.png'
 
 const Footer = () => {
+
+    const handlerScrollUp = () => {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+
+            });
+        }
+    }
     return (
-        <footer>
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.box_logo}>
+                    <img className={styles.btn_scroll_up} onClick={handlerScrollUp} src={logo} alt="logo" />
+                </div>
+                <div className={styles.box_contact}>
+                    <div className={styles.box_contact_info}>
+                        <p id={styles.contact_number}>+996 (552) 343 656</p>
+                    </div>
+                    <div className={styles.box_contact_info}>
+                        <p>Наш адресс:</p>
+                        <p>Кыргызстан, г. Бишкек<br />
+                            ул. Усенбаева №52</p>
+                    </div>
+                    <div className={styles.box_contact_info}>
+                        <p>Наша почта:</p>
+                        <p>Почта: betterlife.kg@gmail.com</p>
+                    </div>
+                    <div className={`${styles.box_contact_info} ${styles.box_contact_icons}`}>
+                        <a href="https://telegram.org/" target="_blank"><img src={telegram} alt="telegram" /></a>
+                        <a href="https://www.whatsapp.com/?lang=ru" target="_blank"><img src={whatsapp} alt="whatsapp" /></a>
+                        <a href="https://www.instagram.com/" target="_blank"><img src={instagram} alt="instagram" /></a>
+                    </div>
+
+                </div>
+            </div>
 
         </footer>
     );
