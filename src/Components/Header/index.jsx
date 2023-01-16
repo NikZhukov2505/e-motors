dc47964e8916c9af95496454b842d8432f5d97e0
NikZhukov2505 <unnormaldj@gmail.com>
2023-01-16 11:32:22 +0600

    header changes

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -14,46 +14,48 @@ const Header = () => {
     const params = useParams()
     // console.log(params);
     return (
-        <header id='top' className={styles.header}>
-            <div className={styles.logo_block}>
-                <WavyLink duration={700} color='#000' to={'/'}><img src={`http://176.126.166.199:8000${info.logo}`} alt="e_motors_logo" /></WavyLink>
-            </div>
-            <nav className={styles.nav}>
-                <ul className={styles.menu}>
-                    <li className={location.pathname == '/' ? styles.activeMenu_item : styles.menu_item}>
-                        <WavyLink duration={700} to={'/'} color={'#000'}>
-                            {/* <NavLink to={'/'} className={({ isActive }) =>
+        <header className={styles.header}>
+            <div id='top' className={styles.header_block}>
+                <div className={styles.logo_block}>
+                    <WavyLink duration={700} color='#000' to={'/'}><img src={`http://176.126.166.199:8000${info.logo}`} alt="e_motors_logo" /></WavyLink>
+                </div>
+                <nav className={styles.nav}>
+                    <ul className={styles.menu}>
+                        <li className={location.pathname == '/' ? styles.activeMenu_item : styles.menu_item}>
+                            <WavyLink duration={700} to={'/'} color={'#000'}>
+                                {/* <NavLink to={'/'} className={({ isActive }) =>
                                 isActive ? styles.activeMenu_item : styles.menu_item
                             }> */}
-                            Главная
-                            {/* </NavLink> */}
-                        </WavyLink>
-                    </li>
-                    <li className={location.pathname == '/catalog' || location.pathname == '/detail-motors/1' ? styles.activeMenu_item : styles.menu_item}>
-                        <WavyLink duration={700} to={'/catalog'} color={'#000'}>
-                            {/* <NavLink className={({ isActive }) =>
+                                Главная
+                                {/* </NavLink> */}
+                            </WavyLink>
+                        </li>
+                        <li className={location.pathname == '/catalog' || location.pathname == '/detail-motors/1' ? styles.activeMenu_item : styles.menu_item}>
+                            <WavyLink duration={700} to={'/catalog'} color={'#000'}>
+                                {/* <NavLink className={({ isActive }) =>
                             isActive ? styles.activeMenu_item : styles.menu_item
                         } to={'/catalog'}> */}
-                            Каталог
-                            {/* </NavLink> */}
-                        </WavyLink>
-                    </li>
-                    <li><HashLink to={'/#about-us'} className={styles.links} >О нас</HashLink></li>
-                    <li><Link
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        activeClass={'active'}
-                        duration={1500} className={styles.links} to='contacts'>Контакты</Link></li>
-                    <li className={location.pathname == '/parts' || location.pathname == '/detail-parts/1' ? styles.activeMenu_item : styles.menu_item}>
-                        <WavyLink duration={700} color={'#000'} to={'/parts'}>дополнительное</WavyLink>
-                    </li>
-                </ul>
-            </nav>
-            <div className={styles.connect_block}>
-                <a href={info.whatsapp}>
-                    <button className={styles.connect_btn}>Связаться с нами</button>
-                </a>
+                                Каталог
+                                {/* </NavLink> */}
+                            </WavyLink>
+                        </li>
+                        <li><HashLink to={'/#about-us'} className={styles.links} >О нас</HashLink></li>
+                        <li><Link
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            activeClass={'active'}
+                            duration={1500} className={styles.links} to='contacts'>Контакты</Link></li>
+                        <li className={location.pathname == '/parts' || location.pathname == '/detail-parts/1' ? styles.activeMenu_item : styles.menu_item}>
+                            <WavyLink duration={700} color={'#000'} to={'/parts'}>дополнительное</WavyLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={styles.connect_block}>
+                    <a href={info.whatsapp}>
+                        <button className={styles.connect_btn}>Связаться с нами</button>
+                    </a>
+                </div>
             </div>
         </header>
     );
